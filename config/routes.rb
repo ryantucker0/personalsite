889cbbe562d9498e50677cfdb7b4fdb0d7debdfa
Ryan Tucker <ryantucker0@gmail.com>
2015-02-09 16:21:41 -0500

    Added new routes and working on navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Personalsite::Application.routes.draw do
   root "pages#home"
+  get "about" => "pages#about"
+  get "projects" => "pages#projects"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
