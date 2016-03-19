ed824820103c4668b9dbcaa8f062f3f1dd5c0f61
ryantucker0 <ryantucker0@gmail.com>
2016-03-19 17:19:45 -0400

    Had to change to rails in order to host using heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Personalsite::Application.config.secret_key_base = '02c8f90f2dca5672dced36b9865c8fdfa7e8e14feb93d03d5d855d7194cb394958242bbcfce1953d03b2f4e49c60c10bbb990268e6dbe841ae77c62f5a7e23fb'
